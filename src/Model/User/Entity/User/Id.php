4d1c4089578c0e38b91d5012c89ec398d7a84fe5
Evgeny <postmail.evg@mail.ru>
2020-10-03 13:34:16 +0300

    Добавление объекта Id для сущности User

---
+++ src/Model/User/Entity/User/Id.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model\User\Entity\User;
+
+use Ramsey\Uuid\Uuid;
+use Webmozart\Assert\Assert;
+
+class Id
+{
+    /**
+     * @var string
+     */
+    private string $value;
+
+    /**
+     * Id constructor.
+     * @param string $value
+     */
+    public function __construct(string $value)
+    {
+        Assert::notEmpty($value);
+        $this->value = $value;
+    }
+
+    /**
+     * @return $this
+     * @throws \Exception
+     */
+    public static function next(): self
+    {
+        return new self(Uuid::uuid4()->toString());
+    }
+    /**
+     * @return string
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    public function __toString(): string
+    {
+        return $this->getValue();
+    }
+}
