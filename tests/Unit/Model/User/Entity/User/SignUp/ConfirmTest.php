144706d2bfee0374160cb52db02fa3677d5be048
evgeny <e.kofman@efko.ru>
2020-10-07 08:11:02 +0300

    Fix tests

--- tests/Unit/Model/User/Entity/User/SignUp/ConfirmTest.php
+++ tests/Unit/Model/User/Entity/User/SignUp/ConfirmTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Tests\Unit\Model\User\Entity\User\SignUp;
 
+use App\Model\User\Entity\User\Email;
 use App\Tests\Builder\User\UserBuilder;
 use PHPUnit\Framework\TestCase;
 
@@ -21,10 +22,10 @@ class ConfirmTest extends TestCase
         self::assertFalse($user->isNew());
         self::assertFalse($user->isWait());
 
-        self::assertEquals($user->getId(), $userBuilder->getId());
-        self::assertEquals($user->getEmail(), $userBuilder->getEmail());
-        self::assertEquals($user->getDate(), $userBuilder->getDate());
-        self::assertEquals($user->getPasswordHash(), $userBuilder->getHash());
+        self::assertEquals($user->getId(), $userBuilder->defaultParams('id'));
+        self::assertEquals($user->getEmail(), new Email($userBuilder->defaultParams('email')));
+        self::assertEquals($user->getDate(), $userBuilder->defaultParams('date'));
+        self::assertEquals($user->getPasswordHash(), $userBuilder->defaultParams('passwordHash'));
         self::assertEquals($user->getConfirmToken(), null); // После подтверждения email сбрасывается
 
         self::assertTrue($user->getRole()->isUser());
