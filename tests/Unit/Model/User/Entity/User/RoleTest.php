4571325775b9129888df31dd8ab37749c9485f11
evgeny <e.kofman@efko.ru>
2020-10-08 13:06:20 +0300

    Тестирование роли

---
+++ tests/Unit/Model/User/Entity/User/RoleTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit\Model\User\Entity\User;
+
+use App\Model\User\Entity\User\Role;
+use App\Tests\Builder\User\UserBuilder;
+use PHPUnit\Framework\TestCase;
+
+class RoleTest extends TestCase
+{
+    public function testSuccess(): void
+    {
+        $userBuilder = new UserBuilder();
+        $user = $userBuilder
+            ->confirmed()
+            ->buildByEmail();
+
+        $user->changeRole(Role::admin());
+
+        self::assertFalse($user->getRole()->isUser());
+        self::assertTrue($user->getRole()->isAdmin());
+    }
+
+    public function testAlready(): void
+    {
+        $userBuilder = new UserBuilder();
+        $user = $userBuilder
+            ->confirmed()
+            ->buildByEmail();
+
+        $this->expectExceptionMessage('Role is already same.');
+        $user->changeRole(Role::user());
+    }
+}
