3c48a179ff209091e7aad1d72fa29037838394a9
Evgeny <postmail.evg@mail.ru>
2020-10-18 23:43:33 +0300

    Signup template complete, correct controller

--- src/Controller/Auth/SignUpController.php
+++ src/Controller/Auth/SignUpController.php
@@ -32,12 +32,6 @@ class SignUpController extends AbstractController
      * @param Request $request
      * @param SignUp\Request\Handler $handler
      * @return Response
-     * @throws \Doctrine\ORM\NoResultException
-     * @throws \Doctrine\ORM\NonUniqueResultException
-     * @throws \Symfony\Component\Mailer\Exception\TransportExceptionInterface
-     * @throws \Twig\Error\LoaderError
-     * @throws \Twig\Error\RuntimeError
-     * @throws \Twig\Error\SyntaxError
      */
     public function request(Request $request, SignUp\Request\Handler $handler): Response
     {
