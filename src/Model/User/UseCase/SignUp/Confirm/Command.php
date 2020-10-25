b50d2286efc89ca7a65a68df1df51299091ba319
Evgeny <Postmail.evg@mail.ru>
2020-10-26 00:09:38 +0300

    Подтверждения email-а через уведомление пользователю

---
+++ src/Model/User/UseCase/SignUp/Confirm/Command.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model\User\UseCase\SignUp\Confirm;
+
+class Command
+{
+    public string $token;
+
+    /**
+     * Command constructor.
+     * @param string $token
+     */
+    public function __construct(string $token)
+    {
+        $this->token = $token;
+    }
+}
