fbed4322e15a290fad729b5b87ddd0d677b4dac3
Evgeny <e.kofman@efko.ru>
2020-10-31 14:24:42 +0300

    Сервис создания письма с отправителем от приложения

--- src/Service/Email/AppTemplateCreator.php
+++ src/Service/Email/AppTemplateCreator.php
@@ -2,22 +2,32 @@
 
 declare(strict_types=1);
 
-namespace App\Container\Common;
+namespace App\Service\Email;
 
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\Mime\Email;
 
-class EmailFromAppFactory
+class AppTemplateCreator
 {
+    private string $from;
+
     /**
-     * Возвращает шаблон письми с уже
-     * установленным отправителем от приложения
-     *
+     * AppTemplateCreator constructor.
      * @param string $from
+     */
+    public function __construct(string $from)
+    {
+        $this->from = $from;
+    }
+
+    /**
+     * Возвращает шаблон письма с уже
+     * установленным отправителем (от приложения)
+     *
      * @return Email
      */
-    public function create(string $from): Email
+    public function getTemplate(): Email
     {
-        return (new TemplatedEmail())->from($from);
+        return (new TemplatedEmail())->from($this->from);
     }
 }
