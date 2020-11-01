1d467aab1c305ad3330c4c041f50feb4dec3fe83
Evgeny <Postmail.evg@mail.ru>
2020-11-01 15:10:10 +0300

    Изменение пароля пользователя после запроса на сброс

--- src/Controller/Auth/ResetController.php
+++ src/Controller/Auth/ResetController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Controller\Auth;
 
 use App\Model\User\UseCase\Reset;
+use App\ReadModel\User\UserFetcher;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -58,10 +59,43 @@ class ResetController extends AbstractController
 
     /**
      * @Route("/reset/{token}", name="auth.reset.reset")
+     * @param string $token
      * @param Request $request
+     * @param Reset\Reset\Handler $handler
+     * @param UserFetcher $users
+     * @return Response
      */
-    public function reset(Request $request): Response
+    public function reset(
+        string $token,
+        Request $request,
+        Reset\Reset\Handler $handler,
+        UserFetcher $users
+    ): Response
     {
+        if (! $users->existByResetToken($token)) {
+            $this->addFlash('error', 'Incorrect or already confirmed token');
+            $this->redirectToRoute('home');
+        }
 
+        $command = new Reset\Reset\Command($token);
+
+        $form = $this->createForm(Reset\Reset\Form::class, $command);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            try {
+                $handler->handle($command);
+                $this->addFlash('success', 'Password is successfully changed.');
+                $this->redirectToRoute('app_login');
+            } catch (\DomainException $e) {
+                $this->logger->error($e->getMessage(), ['exception' => $e]);
+                $this->addFlash('error', $e->getMessage());
+            }
+        }
+
+        return $this->render('app/auth/reset/reset.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
