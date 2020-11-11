1f3bf98596ff9db939d54fcb4e1de06abb292634
Evgeny <Postmail.evg@mail.ru>
2020-11-11 13:07:31 +0300

    Добавление CoreUI assets

--- assets/app.js
+++ assets/app.js
@@ -10,4 +10,4 @@ import './styles/app.scss';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
-require('bootstrap');
+require('@coreui/coreui');
