72125732882b3870e271a904c7051da671db41ce
Evgeny <e.kofman@efko.ru>
2021-01-04 22:58:25 +0300

    Интернационализация страницы входа в систему

---
+++ translations/app.ru.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'Password Reset' => 'Сброс пароля',
+    'Send' => 'Отправить',
+    'To enter, you must specify your email and password.' => 'Для входа необходимо указать email и пароль.',
+    'Login' => 'Вход в систему',
+    'Forgot your password?' => 'Забыли пароль?',
+    'Registration' => 'Регистрация',
+    'To register in the system, follow the link below and fill out the registration form.' => 'Для регистрации в системе перейдите по ссылке расположенной ниже и заполните форму регистрации.',
+    'Register now' => 'Зарегистрироваться сейчас',
+    'Sign up' => 'Регистрация',
+    'Password' => 'Пароль',
+];
