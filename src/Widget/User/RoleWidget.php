93ff9847a895160ecac2b6d7eb88f61191f74b81
Evgeny <Postmail.evg@mail.ru>
2021-01-10 21:12:07 +0300

    Twig виджет для отображения ролей в профиле

---
+++ src/Widget/User/RoleWidget.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Widget\User;
+
+use Twig\Environment;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class RoleWidget extends AbstractExtension
+{
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('user_role', [$this, 'role'], [
+                'needs_environment' => true,
+                'is_safe' => ['html']
+            ])
+        ];
+    }
+
+    /**
+     * @param Environment $twig
+     * @param string $role
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function role(Environment $twig, string $role)
+    {
+        return $twig->render('widget/user/role.html.twig', compact('role'));
+    }
+}
