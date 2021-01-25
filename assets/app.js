30b9776d8ac36c25ed5674be47b2b6c5e5463d52
Evgeny <postmail.evg@mail.ru>
2021-01-26 00:01:33 +0300

    Добавление fontawesome

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,10 @@
-/*
+    /*
  * Welcome to your app's main JavaScript file!
  *
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
+import '@fortawesome/fontawesome-free/js/all';
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
