91b3aea49527a30f51c36e9d477e23a43e23f56b
Evgeny <Postmail.evg@mail.ru>
2021-08-01 23:39:28 +0300

    Create name widget

---
+++ src/Widget/User/NameWidget.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Widget\User;
+
+use App\Security\UserIdentity;
+use Twig\Environment;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class NameWidget extends AbstractExtension
+{
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('user_name', [$this, 'name'], [
+                'needs_environment' => true,
+                'is_safe' => ['html']
+            ])
+        ];
+    }
+
+    /**
+     * @throws \Twig\Error\SyntaxError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\LoaderError
+     */
+    public function name(Environment $twig, UserIdentity $user): string
+    {
+        return $twig->render('widget/user/name.html.twig', compact('user'));
+    }
+}
