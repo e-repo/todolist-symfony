f7ab8b05d2478b07d8653d9e9662dafce60b17cf
Evgeny <postmail.evg@mail.ru>
2022-02-23 23:22:37 +0300

    Creating layout for the admin panel (sidebar and main content)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,39 @@
 <template>
   <div class="wrapper">
-    <aside class="l-sidebar">Side bar</aside>
+    <aside class="l-sidebar">
+      <div class="container-fluid">
+        Side bar
+      </div>
+    </aside>
     <div class="content">
-      <header>Header</header>
-      <footer>Footer</footer>
+      <header class="header">
+        <ul class="nav justify-content-end bg-light border-bottom">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Active</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          </li>
+        </ul>
+      </header>
+      <main class="main">
+        <div class="container-fluid">
+          Main content
+        </div>
+      </main>
+      <footer class="footer bg-light border-top">
+        <div class="container-fluid h-100">
+          <div class="footer__wrapper">
+            <span>Evening production 2022</span>
+          </div>
+        </div>
+      </footer>
     </div>
   </div>
 </template>
