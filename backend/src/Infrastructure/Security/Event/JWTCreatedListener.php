239f467cca398f8c3797b1a2b534809aeccd87a1
Evgeny <postmail.evg@mail.ru>
2022-04-23 17:30:37 +0300

    Add user info to jwt payload

---
+++ backend/src/Infrastructure/Security/Event/JWTCreatedListener.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Security\Event;
+
+use App\Infrastructure\Security\UserIdentity;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class JWTCreatedListener
+{
+    public function onJWTCreated(JWTCreatedEvent $event): void
+    {
+        $payload = $event->getData();
+        $payloadUser['email'] = $payload['username'];
+        $payloadUser['roles'] = $payload['roles'];
+
+        unset($payload['username'], $payload['roles']);
+
+        /** @var UserIdentity $user */
+        $user = $event->getUser();
+
+        if (null !== $user->getName()) {
+            $payloadUser['first'] = $user->getName()->getFirst();
+            $payloadUser['last'] = $user->getName()->getLast();
+            $payloadUser['id'] = $user->getId();
+        }
+
+        $payload['user'] = $payloadUser;
+        $event->setData($payload);
+    }
+}
